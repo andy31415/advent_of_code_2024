5f1920a05216ebe38a2877b655a5108e62de3679
Andrei Litvin <andy314@gmail.com>
2024-12-01 10:34:47 -0500

    Cargo format

--- aoc1/src/lib.rs
+++ aoc1/src/lib.rs
@@ -1,6 +1,5 @@
 use std::iter::zip;
 
-
 #[derive(Debug, PartialEq)]
 struct Input {
     v1: Vec<u32>,
@@ -9,29 +8,29 @@ struct Input {
 
 mod parse {
 
-use nom::{character::complete::{line_ending, multispace1, u32 as parse_u32}, multi::{many0, separated_list1}, sequence::separated_pair, IResult, Parser};
-use nom_supreme::ParserExt;
-use super::Input;
+    use super::Input;
+    use nom::{
+        character::complete::{line_ending, multispace1, u32 as parse_u32},
+        multi::{many0, separated_list1},
+        sequence::separated_pair,
+        IResult, Parser,
+    };
+    use nom_supreme::ParserExt;
 
-pub(crate) fn pair(input: &str) -> IResult<&str, (u32, u32)> {
-    separated_pair(parse_u32, multispace1 , parse_u32)
-        .parse(input)
+    pub(crate) fn pair(input: &str) -> IResult<&str, (u32, u32)> {
+        separated_pair(parse_u32, multispace1, parse_u32).parse(input)
+    }
+
+    pub fn input(input: &str) -> IResult<&str, Input> {
+        separated_list1(line_ending, pair)
+            .terminated(many0(line_ending))
+            .map(|pairs| Input {
+                v1: pairs.iter().map(|v| v.0).collect(),
+                v2: pairs.iter().map(|v| v.1).collect(),
+            })
+            .parse(input)
+    }
 }
-
-pub fn input(input: &str) -> IResult<&str, Input> {
-   separated_list1(line_ending, pair)
-       .terminated(many0(line_ending))
-       .map(|pairs| {
-           Input{
-               v1: pairs.iter().map(|v| {v.0}).collect(),
-               v2: pairs.iter().map(|v| {v.1}).collect(),
-           }
-       })
-       .parse(input)
-}
-
-}
-
 
 pub fn part1(input: &str) -> u32 {
     let (r, mut d) = parse::input(input).expect("Valid input");
@@ -40,9 +39,7 @@ pub fn part1(input: &str) -> u32 {
     d.v1.sort();
     d.v2.sort();
 
-    zip(d.v1, d.v2).fold(0u32, |v, p| {
-        v + p.1.abs_diff(p.0)
-    })
+    zip(d.v1, d.v2).fold(0u32, |v, p| v + p.1.abs_diff(p.0))
 }
 
 pub fn part2(_input: &str) -> usize {
@@ -63,10 +60,13 @@ mod tests {
 
     #[test]
     fn test_parse_input() {
-        assert_eq!(parse::input("1 2\n3 4").expect("valid").1, Input {
-            v1: vec![1u32, 3u32],
-            v2: vec![2u32, 4u32],
-        });
+        assert_eq!(
+            parse::input("1 2\n3 4").expect("valid").1,
+            Input {
+                v1: vec![1u32, 3u32],
+                v2: vec![2u32, 4u32],
+            }
+        );
     }
 
     #[test]
