e3edb581b78da4d76967e66b37f11fb6c336e9c7
Andrei Litvin <andy314@gmail.com>
2024-12-05 19:13:16 -0500

    Add day 5 starter/empty

---
+++ aoc5/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn part1(input: &str) -> usize {
+    // TODO: implement
+    0
+}
+
+pub fn part2(input: &str) -> usize {
+    // TODO: implement
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(include_str!("../example.txt")), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(include_str!("../example.txt")), 0);
+    }
+}
