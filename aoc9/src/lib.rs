ee0df4c28f9b8378deb58e93abf53caeafb7c4a5
Andrei Litvin <andy314@gmail.com>
2024-12-09 20:16:05 -0500

    Part 2 done ... ugly way

--- aoc9/src/lib.rs
+++ aoc9/src/lib.rs
@@ -175,9 +175,98 @@ pub fn part1(input: &str) -> usize {
         .sum()
 }
 
+// Content for disk: free or file with index
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum BlockContent {
+    File(usize),
+    Free,
+}
+
 pub fn part2(input: &str) -> usize {
     // TODO: implement
-    0
+    let (r, d) = parsing::flat_disk(input).expect("valid input");
+    assert!(r.is_empty());
+
+    tracing::info!("Allocated: {}", d.allocated());
+
+    // File IDs only
+    let mut blocks = Vec::new();
+    let mut id = 0;
+    for f in &d.locations {
+        match f {
+            DiskLocation::File(n) => {
+                for _ in 0..*n {
+                    blocks.push(BlockContent::File(id));
+                }
+                id += 1;
+            }
+            DiskLocation::Free(n) => {
+                for _ in 0..*n {
+                    blocks.push(BlockContent::Free);
+                }
+            }
+        }
+    }
+
+    tracing::info!("BLOCKS: {:?}", blocks);
+
+    for end_id in (0..id).rev() {
+        // TRY TO MOVE THE FILE WITH THE GIVEN ID:
+        let start_idx = blocks
+            .iter()
+            .enumerate()
+            .find(|(idx, value)| **value == BlockContent::File(end_id))
+            .expect("Files exist")
+            .0;
+
+        let mut file_size = 1;
+        while start_idx + file_size < blocks.len()
+            && blocks.get(start_idx + file_size) == Some(&BlockContent::File(end_id))
+        {
+            file_size += 1;
+        }
+
+        tracing::info!(
+            "  {:?} starts at {:?} and has length {:?}",
+            end_id,
+            start_idx,
+            file_size
+        );
+
+        // find a free space of at least len items
+        let mut free_pos = None;
+        for (idx, window) in (&blocks).windows(file_size).enumerate() {
+            if window.iter().all(|b| *b == BlockContent::Free) {
+                tracing::info!("FOUND FREE");
+                free_pos = Some(idx);
+                break;
+            }
+            if idx >= start_idx {
+                tracing::info!("REACHED THE END");
+                break;
+            }
+        }
+        if let Some(pos) = free_pos {
+            tracing::info!("SUITABLE SPACE: {:?}", pos);
+
+            for n in 0..file_size {
+                blocks[pos + n] = blocks[start_idx + n];
+                blocks[start_idx + n] = BlockContent::Free;
+            }
+            tracing::info!("BLOCKS: {:?}", blocks);
+        } else {
+            tracing::info!("Cannot move");
+        }
+    }
+
+    blocks
+        .iter()
+        .enumerate()
+        .map(|(idx, b)| match b {
+            BlockContent::File(n) => n * idx,
+            BlockContent::Free => 0,
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -207,7 +296,8 @@ mod tests {
     }
 
     #[test]
+    #[tracing_test::traced_test]
     fn test_part2() {
-        assert_eq!(part2(include_str!("../example.txt")), 0);
+        assert_eq!(part2(include_str!("../example.txt")), 2858);
     }
 }
