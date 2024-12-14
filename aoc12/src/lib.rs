ae7b398c4610a73530ee9b0a1acc4f78dc4f8b85
Andrei Litvin <andy314@gmail.com>
2024-12-13 20:35:18 -0500

    Map parse

--- aoc12/src/lib.rs
+++ aoc12/src/lib.rs
@@ -1,4 +1,24 @@
+use map_parse::Parseable;
+use nom::{character::complete::satisfy, Parser};
+
+#[derive(Debug, PartialEq, Copy, Clone, PartialOrd, Eq, Ord)]
+enum Plant {
+    Value(char),
+}
+
+impl Parseable for Plant {
+    type Item = Plant;
+
+    fn parse(s: &str) -> nom::IResult<&str, Self::Item> {
+        satisfy(|c| c != '\r' && c != '\n')
+            .map(Plant::Value)
+            .parse(s)
+    }
+}
+
 pub fn part1(input: &str) -> usize {
+    let (r, m) = map_parse::Map::<Plant>::parse(input).expect("Valid input");
+    assert!(r.is_empty());
     // TODO: implement
     0
 }
@@ -14,7 +34,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(include_str!("../example.txt")), 0);
+        assert_eq!(part1(include_str!("../example.txt")), 1930);
     }
 
     #[test]
