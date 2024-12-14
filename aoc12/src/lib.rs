c0c03c58947c6bbbc10936083d31ab3de3c51919
Andrei Litvin <andy314@gmail.com>
2024-12-13 20:52:17 -0500

    part 1 done

--- aoc12/src/lib.rs
+++ aoc12/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+use glam::IVec2;
 use map_parse::Parseable;
 use nom::{character::complete::satisfy, Parser};
 
@@ -16,11 +19,72 @@ impl Parseable for Plant {
     }
 }
 
+fn p1_cost(
+    start_position: &IVec2,
+    garden: &map_parse::Map<Plant>,
+    seen: &mut HashSet<IVec2>,
+) -> usize {
+    if seen.contains(start_position) {
+        return 0;
+    }
+
+    let plant = garden
+        .get(start_position)
+        .expect("Starting position is valid");
+
+    let mut to_check = VecDeque::new();
+
+    seen.insert(*start_position);
+    to_check.push_back(*start_position);
+
+    let mut walls = 0;
+    let mut cells = 0;
+
+    while let Some(value) = to_check.pop_front() {
+        cells += 1;
+
+        for dir in [
+            IVec2::new(0, 1),
+            IVec2::new(0, -1),
+            IVec2::new(1, 0),
+            IVec2::new(-1, 0),
+        ] {
+            let new_pos = value + dir;
+
+            if !garden.is_inside(new_pos) {
+                walls += 1;
+                continue;
+            }
+
+            if garden.get(&new_pos).expect("valid point") == plant {
+                if !seen.contains(&new_pos) {
+                    seen.insert(new_pos);
+                    to_check.push_back(new_pos);
+                }
+            } else {
+                walls += 1;
+            }
+        }
+    }
+    tracing::info!(
+        "Starting at {:?} cell {:?} has {} walls and {} cells",
+        start_position,
+        plant,
+        walls,
+        cells
+    );
+    walls * cells
+}
+
 pub fn part1(input: &str) -> usize {
     let (r, m) = map_parse::Map::<Plant>::parse(input).expect("Valid input");
     assert!(r.is_empty());
-    // TODO: implement
-    0
+
+    let mut seen = HashSet::new();
+
+    m.values_iter()
+        .map(|(pos, _)| p1_cost(pos, &m, &mut seen))
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -33,6 +97,7 @@ mod tests {
     use super::*;
 
     #[test]
+    #[tracing_test::traced_test]
     fn test_part1() {
         assert_eq!(part1(include_str!("../example.txt")), 1930);
     }
