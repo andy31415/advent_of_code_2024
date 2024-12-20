1a5aeebea20a8d1212ade1c1c75192e1a69456b1
Andrei Litvin <andy314@gmail.com>
2024-12-20 14:57:42 -0500

    First cost works

--- aoc20/src/lib.rs
+++ aoc20/src/lib.rs
@@ -1,3 +1,9 @@
+use std::collections::HashSet;
+
+use glam::IVec2;
+use nom::{branch::alt, bytes::complete::tag, character::complete::satisfy, Parser};
+use nom_supreme::ParserExt;
+
 #[derive(thiserror::Error, Debug, PartialEq)]
 enum InputParseError {
     #[error("Failed to parse using Nom")]
@@ -6,17 +12,67 @@ enum InputParseError {
     #[error("Unparsed data remained: {0:?}")]
     UnparsedData(String),
 }
+use pathfinding::prelude::dijkstra;
 
-struct Input {}
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Hash, Debug)]
+enum Cell {
+    Wall,
+    Empty,
+    Start,
+    End,
+}
+
+impl map_parse::Parseable for Cell {
+    type Item = Cell;
+
+    fn parse(s: &str) -> nom::IResult<&str, Self::Item> {
+        alt((
+            tag("#").value(Cell::Wall),
+            tag(".").value(Cell::Empty),
+            tag("S").value(Cell::Start),
+            tag("E").value(Cell::End),
+        ))
+        .parse(s)
+    }
+}
+
+struct Input {
+    start: IVec2,
+    end: IVec2,
+    walls: HashSet<IVec2>,
+    rows: usize,
+    cols: usize,
+}
 
 fn parse_input(s: &str) -> Result<Input, InputParseError> {
-    let rest = s;
-
+    let (rest, map) = map_parse::Map::<Cell>::parse(s)?;
     if !rest.is_empty() {
         return Err(InputParseError::UnparsedData(rest.into()));
     }
 
-    Ok(Input {})
+    let start = map
+        .values_iter()
+        .find(|(_, c)| **c == Cell::Start)
+        .map(|(p, _)| *p)
+        .expect("has start");
+    let end = map
+        .values_iter()
+        .find(|(_, c)| **c == Cell::End)
+        .map(|(p, _)| *p)
+        .expect("has start");
+    let walls = map
+        .values_iter()
+        .filter(|(_, c)| **c == Cell::Wall)
+        .map(|(p, _)| *p)
+        .collect();
+
+    Ok(Input {
+        start,
+        end,
+        walls,
+        rows: map.row_count(),
+        cols: map.col_count(),
+    })
 }
 
 impl<INNER: Into<String>> From<nom::Err<nom::error::Error<INNER>>> for InputParseError {
@@ -27,6 +83,26 @@ impl<INNER: Into<String>> From<nom::Err<nom::error::Error<INNER>>> for InputPars
 
 pub fn part1(input: &str) -> color_eyre::Result<usize> {
     let mut input = parse_input(input)?;
+
+    let start_cost = dijkstra(
+        &input.start,
+        |start| {
+            [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                .into_iter()
+                .map(|(x, y)| start + IVec2::new(x, y))
+                .filter(|p| {
+                    p.x >= 0 && (p.x as usize) < input.cols && p.y >= 0 && (p.y as usize) < input.rows && !input.walls.contains(p)
+                })
+                .map(|p| (p, 1))
+                .collect::<Vec<_>>()
+        },
+        |x| x == &input.end,
+    )
+    .expect("Has path")
+    .1;
+
+    println!("START COST: {}", start_cost);
+    // FIXME: dijskstra first, figure out cost
 
     todo!();
 }
