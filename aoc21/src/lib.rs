9a0f23001c37070a23d3f624a9b9f5a57ad3fa8a
Andrei Litvin <andy314@gmail.com>
2024-12-22 12:17:09 -0500

    Unit test and fixes, unit test passes (but not part 1 ... annnoying)

--- aoc21/src/lib.rs
+++ aoc21/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
 use glam::IVec2;
 use nom::{
@@ -45,55 +45,119 @@ impl<INNER: Into<String>> From<nom::Err<nom::error::Error<INNER>>> for InputPars
 }
 
 struct KeyPad {
-    key_coord: HashMap<char, IVec2>,
-    remote_coord: HashMap<char, IVec2>,
+    coord: HashMap<char, IVec2>,
+    gap: IVec2, // where is the gap in the keyboard
 }
 
 impl KeyPad {
-    fn new() -> Self {
-        let mut key_coord = HashMap::new();
+    fn new_button_pad() -> Self {
+        let mut coord = HashMap::new();
 
-        key_coord.insert('7', (0, 0).into());
-        key_coord.insert('8', (1, 0).into());
-        key_coord.insert('9', (2, 0).into());
-        key_coord.insert('4', (0, 1).into());
-        key_coord.insert('5', (1, 1).into());
-        key_coord.insert('6', (2, 2).into());
-        key_coord.insert('1', (0, 2).into());
-        key_coord.insert('2', (1, 2).into());
-        key_coord.insert('3', (2, 2).into());
-        key_coord.insert('0', (1, 3).into());
-        key_coord.insert('A', (2, 3).into());
-
-        let mut remote_coord = HashMap::new();
-
-        remote_coord.insert('^', (1, 0).into());
-        remote_coord.insert('A', (2, 0).into());
-        remote_coord.insert('<', (0, 1).into());
-        remote_coord.insert('v', (1, 1).into());
-        remote_coord.insert('>', (2, 1).into());
+        coord.insert('7', (0, 0).into());
+        coord.insert('8', (1, 0).into());
+        coord.insert('9', (2, 0).into());
+        coord.insert('4', (0, 1).into());
+        coord.insert('5', (1, 1).into());
+        coord.insert('6', (2, 1).into());
+        coord.insert('1', (0, 2).into());
+        coord.insert('2', (1, 2).into());
+        coord.insert('3', (2, 2).into());
+        coord.insert('0', (1, 3).into());
+        coord.insert('A', (2, 3).into());
 
         Self {
-            key_coord,
-            remote_coord,
+            coord,
+            gap: IVec2::new(0, 3),
         }
     }
+
+    fn new_arrow_pad() -> Self {
+        let mut coord = HashMap::new();
+
+        coord.insert('^', (1, 0).into());
+        coord.insert('A', (2, 0).into());
+        coord.insert('<', (0, 1).into());
+        coord.insert('v', (1, 1).into());
+        coord.insert('>', (2, 1).into());
+
+        Self {
+            coord,
+            gap: IVec2::new(0, 0),
+        }
+    }
+
+    fn short_key_path(&self, target: &str) -> String {
+        let mut pos = *self.coord.get(&'A').expect("A has a position");
+
+        let mut moves = String::new();
+
+        for c in target.chars() {
+            let dest = *self
+                .coord
+                .get(&c)
+                .unwrap_or_else(|| panic!("{} MUST BE A valid destination", c));
+
+            let x_moves = if dest.x > pos.x {
+                vec!['>'; (dest.x - pos.x) as usize]
+            } else {
+                vec!['<'; (pos.x - dest.x) as usize]
+            };
+
+            let y_moves = if pos.y > dest.y {
+                vec!['^'; (pos.y - dest.y) as usize]
+            } else {
+                vec!['v'; (dest.y - pos.y) as usize]
+            };
+
+            // figure out how to get there. We do not want to hit the gap
+            if self.gap.y == pos.y {
+                moves.extend(y_moves);
+                moves.extend(x_moves);
+            } else {
+                moves.extend(x_moves);
+                moves.extend(y_moves);
+            }
+
+            moves.push('A');
+
+            pos = dest;
+        }
+
+        moves
+    }
+}
+
+fn code_number(target: &str) -> usize {
+    assert_eq!(target.chars().count(), 4);
+    assert_eq!(target.chars().last(), Some('A'));
+    target.split_at(3).0.parse::<usize>().expect("valid number")
 }
 
 pub fn part1(input: &str) -> color_eyre::Result<usize> {
     let input = parse_input(input)?;
 
-    let keypad = KeyPad::new();
+    let keypad = KeyPad::new_button_pad();
+    let arrow_pad = KeyPad::new_arrow_pad();
 
-    println!("INPUT: {:#?}", input);
+    Ok(input
+        .inputs
+        .iter()
+        .map(|code| {
+            let number = code_number(code);
+            let mincode =
+                arrow_pad.short_key_path(&arrow_pad.short_key_path(&keypad.short_key_path(code)));
 
-    todo!();
+            println!("{}: CODE {} and mincode {}", code, number, mincode.len());
+
+            number * mincode.len()
+        })
+        .sum())
 }
 
 pub fn part2(input: &str) -> color_eyre::Result<usize> {
-    let mut input = parse_input(input)?;
+    let input = parse_input(input)?;
 
-    todo!();
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -111,7 +175,10 @@ mod tests {
     #[test]
     fn test_part1() {
         init_tests();
-        assert_eq!(part1(include_str!("../example.txt")).expect("success"), 0);
+        assert_eq!(
+            part1(include_str!("../example.txt")).expect("success"),
+            126384
+        );
     }
 
     #[test]
