c350cc9ec81159b618709f87c48f7757603207a0
Andrei Litvin <andy314@gmail.com>
2024-12-23 12:34:00 -0500

    Done part 1 ... probably too slow

--- aoc22/src/lib.rs
+++ aoc22/src/lib.rs
@@ -1,3 +1,10 @@
+use nom::{
+    character::complete::{self, line_ending},
+    multi::{many0, separated_list1},
+    Parser as _,
+};
+use nom_supreme::ParserExt;
+
 #[derive(thiserror::Error, Debug, PartialEq)]
 enum ProcessingError {
     #[error("Failed to parse using Nom")]
@@ -7,16 +14,16 @@ enum ProcessingError {
     UnparsedData(String),
 }
 
-struct Input {}
-
-fn parse_input(s: &str) -> Result<Input, ProcessingError> {
-    let rest = s;
+fn parse_input(s: &str) -> Result<Vec<u32>, ProcessingError> {
+    let (rest, values) = separated_list1(line_ending, complete::u32)
+        .terminated(many0(line_ending))
+        .parse(s)?;
 
     if !rest.is_empty() {
         return Err(ProcessingError::UnparsedData(rest.into()));
     }
 
-    Ok(Input {})
+    Ok(values)
 }
 
 impl<INNER: Into<String>> From<nom::Err<nom::error::Error<INNER>>> for ProcessingError {
@@ -25,16 +32,37 @@ impl<INNER: Into<String>> From<nom::Err<nom::error::Error<INNER>>> for Processin
     }
 }
 
-pub fn part1(input: &str) -> color_eyre::Result<usize> {
-    let mut input = parse_input(input)?;
+fn next(value: u32) -> u32 {
+    let value = ((value << 6) ^ value) & 0xFF_FFFF;
+    let value = ((value >> 5) ^ value) & 0xFF_FFFF;
+    let value = ((value << 11) ^ value) & 0xFF_FFFF;
 
-    todo!();
+    value
 }
 
-pub fn part2(input: &str) -> color_eyre::Result<usize> {
-    let mut input = parse_input(input)?;
+fn nth_next(value: u32, cnt: u32) -> u32 {
+    let mut v = value;
+    for _ in 0..cnt {
+        v = next(v);
+    }
+    v
+}
 
-    todo!();
+pub fn part1(input: &str) -> color_eyre::Result<usize> {
+    let input = parse_input(input)?;
+
+    let mut cnt = 0;
+    for v in input {
+        cnt += nth_next(v, 2000) as usize;
+    }
+
+    Ok(cnt)
+}
+
+pub fn part2(input: &str) -> color_eyre::Result<u32> {
+    let input = parse_input(input)?;
+
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -50,9 +78,20 @@ mod tests {
     }
 
     #[test]
+    fn test_next() {
+        init_tests();
+        assert_eq!(next(123), 15887950);
+        assert_eq!(next(15887950), 16495136);
+        assert_eq!(next(16495136), 527345);
+    }
+
+    #[test]
     fn test_part1() {
         init_tests();
-        assert_eq!(part1(include_str!("../example.txt")).expect("success"), 0);
+        assert_eq!(
+            part1(include_str!("../example.txt")).expect("success"),
+            37327623
+        );
     }
 
     #[test]
